98df962650c9bfa3e4c1a33043696a74a15aacd5
Henrik Kryger Høltzer <HEHO@zealand.dk>
2024-11-07 15:52:55 +0100

    Class Pizza Created

---
+++ PizzeriaBigMamma/Pizza.cs
@@ -0,0 +1,25 @@
+﻿namespace PizzeriaBigMamma
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+        public Pizza() { }
+        public Pizza(int id, string name, double price, string toppings)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Description = toppings;
+        }
+
+        public override string ToString()
+        {
+            //return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Name)}={Name}, {nameof(Price)}={Price.ToString()}, {nameof(Description)}={Description}}}";
+            return $"Pizza #{Id.ToString()} {Name}({Description}) af {Price.ToString()}Kr. ";
+        }
+    }
+}
