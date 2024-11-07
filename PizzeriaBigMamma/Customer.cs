b82c63a62abeb7c722d677a2a5dabede6a8a0c7a
Henrik Kryger Høltzer <HEHO@zealand.dk>
2024-11-07 16:31:02 +0100

    Class Customer created

---
+++ PizzeriaBigMamma/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaBigMamma
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        public Customer() { }
+        public Customer(int id, string name, string address, string phone)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Name)}={Name}, {nameof(Address)}={Address}, {nameof(Phone)}={Phone}}}";
+        }
+    }
+}
