6888bf75e5a54987395df9d7bfcd089b19034aef
Henrik Kryger Høltzer <HEHO@zealand.dk>
2024-11-08 13:14:58 +0100

    More Classes is added

---
+++ PizzeriaBigMamma/CustomerRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaBigMamma
+{
+    public class CustomerRepository
+    {
+        private List<Customer> _customers = new List<Customer>();
+
+        public void AddCustomer(Customer customer) { _customers.Add(customer); }
+
+        public Customer GetCustomer(int id)
+        {
+            foreach (var customer in _customers) { if (customer.Id == id) return customer; }
+            return null;
+        }
+
+        public bool UpdateCustomer(int id, Customer customer)
+        {
+            foreach (var c in _customers)
+            {
+                if (c.Id == id)
+                {
+                    c.Name = customer.Name;
+                    c.Address = customer.Address;
+                    c.Phone = customer.Phone;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Customer DeleteCustomer(int id)
+        {
+            foreach (var c in _customers)
+            {
+                if (c.Id == id) _customers.Remove(c);
+                return c;
+            }
+            return null;
+        }
+
+        public List<Customer> GetCustomers() { return _customers; }
+    }
+}
---
+++ PizzeriaBigMamma/Store.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaBigMamma
+{
+    public class Store
+    {
+        public CustomerRepository CustomerRepository { get; private set; } = new CustomerRepository();
+        public PizzaMenuCard PizzaMenuCard { get; private set; } = new PizzaMenuCard();
+        public List<Order> Orders { get; private set; } = new List<Order>();
+
+        public void AddOrder(Order nweOrder)
+        {
+            Orders.Add(nweOrder);
+        }
+
+        public Order GetOrder(int id)
+        {
+            foreach (Order order in Orders)
+                if (order.Id == id) return order;
+            return null;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return Orders;
+        }
+
+        public Order DeleteOrder(int id)
+        {
+            foreach (Order order in Orders)
+            {
+                if (order.Id == id)
+                {
+                    Orders.Remove(order);
+                    return order;
+                }
+            }
+
+            return null;
+        }
+
+
+    }
+}
